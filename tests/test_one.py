19614608a0065388d1d00fe762ccc623b789e8d9
Russell006 <russell.shamim@gmail.com>
2026-02-13 22:00:41 +0100

    skapat klass och testar fil

--- tests/test_one.py
+++ tests/test_one.py
@@ -1,3 +1,11 @@
-def test_one(self):
-    print("Hello World")
-    assert 1 == 1
+
+class TestOne:
+    def test_one(self):
+        print("Hello World!")
+        assert 1 == 1
+
+
+    def test_that_file_data_is_correct(self):
+        with open("testdata/hello.txt") as f:
+            data = f.read()
+            assert data == "Hello World!"
